64086220e4ba98634a0279aacdd1e7db8b767dfa
Pocheva Aza <phvaza@gmail.com>
2025-12-24 22:21:01 +0100

    fixed to see layout

--- src/pages/browseWalkers.vue
+++ src/pages/browseWalkers.vue
@@ -1,3 +1,18 @@
 <template>
-  <h2>Browse Walkers</h2>
+  <section>
+    <h2>Available Dog Walkers</h2>
+
+    <div class="walkers-grid">
+      <div class="walker-card" v-for="n in 3" :key="n">
+        <h3>Walker Name</h3>
+        <p>City: Antwerp</p>
+        <p>€15 / 30 min</p>
+        <p>⭐ 4.8</p>
+
+        <router-link class="btn small" to="#">
+          View profile
+        </router-link>
+      </div>
+    </div>
+  </section>
 </template>
